d5d982ac44f9cfa7c973bdd032ddd7241feae141
Derveljun <derveljun@tistory.com>
2020-02-04 17:49:17 +0900

    # 다중 폴더처리를 할 수 있도록 ListView 처리

--- pdf-manager/src/main/java/com/derveljun/jasmine/pdfmanager/fx/PdfMainController.java
+++ pdf-manager/src/main/java/com/derveljun/jasmine/pdfmanager/fx/PdfMainController.java
@@ -5,18 +5,22 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import lombok.Data;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 @Data
 @Service
@@ -26,19 +30,18 @@ public class PdfMainController implements Initializable {
     private Stage stage;
 
     @FXML
-    private Button btnSourceDirSearch,
-                btnTargetDirSearch,
-                btnSave;
+    private ListView lvTargetDirs;
 
     @FXML
-    private TextField txtSourceDir, txtTargetDir, txtFileName;
+    private Button btnTargetDirSearch, btnSave, btnDirsChoose;
 
     @FXML
-    public TextArea txtAreaLog;
+    private TextField txtTargetDir;
 
     private PdfService pdfService;
     private Alert alertWarning;
     private DirectoryChooser directoryChooser;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -47,35 +50,47 @@ public class PdfMainController implements Initializable {
         alertWarning = new Alert(Alert.AlertType.WARNING);
         directoryChooser = new DirectoryChooser();
 
-        btnSourceDirSearch.setOnAction(e -> {
-            File selectedFile = directoryChooser.showDialog(stage);
-            txtSourceDir.setText(selectedFile.getAbsolutePath());
-        });
-
         btnTargetDirSearch.setOnAction(e -> {
             File selectedFile = directoryChooser.showDialog(stage);
-            txtTargetDir.setText(selectedFile.getAbsolutePath());
+            txtTargetDir.setText(selectedFile.toString());
+        });
+
+        btnDirsChoose.setOnAction(e -> {
+            File selectedFile = directoryChooser.showDialog(stage);
+            try {
+                List<String> dirs = Files.walk(Paths.get(selectedFile.getAbsolutePath()))
+                        .filter(Files::isDirectory)
+                        .map(f -> f.toString())
+                        .collect(Collectors.toList());
+                if (dirs == null || dirs.isEmpty() || dirs.size() == 1) {
+                    alert("선택 된 폴더가 없습니다.");
+                    return;
+                }
+
+                dirs.remove(0);
+                lvTargetDirs.getItems().removeAll();
+                lvTargetDirs.getItems().addAll(dirs);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         });
 
         btnSave.setOnAction(e -> {
 
-            String sourceDir = txtSourceDir.getText();
             String targetDir = txtTargetDir.getText();
-            String fileName = txtFileName.getText();
-
-            if (StringUtils.isEmpty(sourceDir)
-                    || StringUtils.isEmpty(targetDir)
-                    || StringUtils.isEmpty(fileName)) {
-                alert("Select a directory or write a File name");
-                return;
-            }
+            List<String> dirs = lvTargetDirs.getItems();
 
             try {
-                pdfService.images2Pdf(sourceDir, targetDir, fileName);
+                for (String sourceDir : dirs) {
+                    String[] splt = sourceDir.split("/");
+                    String pdfFileName = splt[splt.length - 1];
+
+                    pdfService.images2Pdf(sourceDir, targetDir, pdfFileName);
+                }
 
                 // TODO rxJava로 바꾸기
+                // TODO 진행 표시 : Listview 옆에 진행률 표시
                 // TODO 완료 표시처리
-                // TODO 진행 표시
                 // TODO 폴더 리스트로 변경
 
             } catch (Exception ex) {
